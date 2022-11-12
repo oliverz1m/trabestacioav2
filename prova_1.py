f6994aedd345913687757792e08e817f3e50ab27
Gabriel Nascimento <oliverzito000@gmail.com>
2022-11-11 21:52:39 -0300

    first commit

---
+++ prova_1.py
@@ -0,0 +1,6 @@
+bmai = float(input('Digite a base maior: '))
+bmen = float(input('Digite a base menor: '))
+alt = float(input('Digite a altura: '))
+calc = (bmai + bmen)*alt
+div = calc/2
+print('A é igual a: {}'.format(div))
